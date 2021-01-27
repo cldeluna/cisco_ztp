b497a94ed088922a21948b42e8767b16d04a3fb4
Claudia de Luna <claudia@indigowire.net>
2021-01-27 06:46:19 -0800

    Updated to configure g0/0

--- ztp_python_base.py
+++ ztp_python_base.py
@@ -8,8 +8,9 @@ import cli
 print "\n\n *** Executing show version *** \n\n"
 cli.executep('show version')
 
-print "\n\n *** Configuring a Loopback Interface *** \n\n"
-cli.configurep(["interface loop 100", "ip address 10.10.10.10 255.255.255.255", "end"])
+print "\n\n *** Configuring Management Interface *** \n\n"
+# cli.configurep(["interface loop 100", "ip address 10.10.10.10 255.255.255.255", "end"])
+cli.clip("configure terminal ; interface GigabitEthernet0/0 ; description MGMT_INT_BASE ; ip address dhcp ; no shutdown")
 
 print "\n\n *** Executing show ip interface brief *** \n\n"
 cli.executep('show ip int brief')
