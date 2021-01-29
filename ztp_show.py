338c04a9287622b73f212e4d6133592ace9fff58
Claudia de Luna <claudia@indigowire.net>
2021-01-29 06:51:40 -0800

    getting hostname

--- ztp_show.py
+++ ztp_show.py
@@ -147,7 +147,7 @@ def main():
             else:
                 cmds = cmd_dict['general_show_commands']
             resp = utils.conn_and_get_output(devdict, cmds)
-            print(resp)
+            # print(resp)
             output_dir = os.path.join(os.getcwd(), arguments.output_subdir, f"{dev}.txt")
             utils.write_txt(output_dir, resp)
 
